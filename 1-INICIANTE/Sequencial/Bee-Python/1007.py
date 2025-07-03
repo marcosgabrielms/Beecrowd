98e121bcb400deaf0059e11f7770132e9d3969e6
Marcos <marcosgabriel166@gmail.com>
2025-07-02 22:45:41 -0300

    :sparkles: feat: adiciona questão 1007 BeeCrowd
    -> Linguagem: Python
    -> Assunto: Sequencial

---
+++ 1-INICIANTE/Sequencial/Bee-Python/1007.py
@@ -0,0 +1,13 @@
+def main():
+
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+
+    diferenca = (a * b - c * d)
+
+    print(f"DIFERENCA = {diferenca}")
+
+if __name__ == '__main__':
+    main()
