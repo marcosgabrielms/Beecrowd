2cfc716360e2e31f8bb2b0e8820baee27e365efe
Marcos <marcosgabriel166@gmail.com>
2025-07-02 22:46:14 -0300

    :sparkles: feat: adiciona questão 1011 BeeCrowd
    -> Linguagem: Python
    -> Assunto: Sequencial

---
+++ 1-INICIANTE/Sequencial/Bee-Python/1011.py
@@ -0,0 +1,10 @@
+def main():
+
+    raio = float(input())
+    valor_pi = 3.14159
+    volume_esfera = (4/3) * valor_pi * (raio**3)
+
+    print(f"VOLUME = {volume_esfera:.3f}")
+
+if __name__ == '__main__':
+    main()
